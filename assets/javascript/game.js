1a4439d5d816a1e11f53f64c7580cdff5305453f
Hayden Wampach <wampachhayden@gmail.com>
2019-09-20 20:11:17 -0700

    added css style, resized images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
+    
     //variables
     var wins = 0;
     var losses = 0;
     var userTotalPoints = 0;
     var targetScore = Math.floor(Math.random() * 101 + 19);
-    var gem1 = Math.floor(Math.random() * 11 + 1)
-    var gem2 = Math.floor(Math.random() * 11 + 1)
-    var gem3 = Math.floor(Math.random() * 11 + 1)
-    var gem4 = Math.floor(Math.random() * 11 + 1)
+    var gem1 = Math.floor(Math.random() * 11 + 1);
+    var gem2 = Math.floor(Math.random() * 11 + 1);
+    var gem3 = Math.floor(Math.random() * 11 + 1);
+    var gem4 = Math.floor(Math.random() * 11 + 1);
 
     // var to html tag
     $('#goalScore').text(targetScore);
     $('winCount').text(wins);
-    $('#lossCount').text(losses)
+    $('#lossCount').text(losses);
     $('#totalPoints').text(userTotalPoints);
     $('#alert').text(' ');
 
@@ -70,10 +71,10 @@ $(document).ready(function() {
         userTotalPoints = 0;
         targetScore = Math.floor(Math.random() * 101 + 19);
         $('#goalScore').text(targetScore);
-        gem1 = Math.floor(Math.random()*11+1)
-        gem2 = Math.floor(Math.random()*11+1)
-        gem3 = Math.floor(Math.random()*11+1)
-        gem4 = Math.floor(Math.random()*11+1)
+        gem1 = Math.floor(Math.random() *11+1);
+        gem2 = Math.floor(Math.random()*11+1);
+        gem3 = Math.floor(Math.random()*11+1);
+        gem4 = Math.floor(Math.random()*11+1);
     }
 
 })
